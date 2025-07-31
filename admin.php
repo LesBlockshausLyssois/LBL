415c459c99aa7d4aa8d3eb12bc1fdd808ed686c1
LesBlockshausLyssois <ipad.gm.0209@gmail.com>
2025-07-31 14:34:27 +0200

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,12 @@
+<?php
+$correct_code = "LBL2025"; // <-- Ton code ici
+
+if (isset($_GET['code']) && $_GET['code'] === $correct_code) {
+    // Redirection vers la vraie page admin
+    header("Location: ADMIN.html");
+    exit();
+} else {
+    echo "<script>alert('Code incorrect.'); window.location.href = 'index.html';</script>";
+    exit();
+}
+?>
