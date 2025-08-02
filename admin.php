8789d4eb131aa8bf32a97f76eef1d3209c5e4e73
LesBlockshausLyssois <ipad.gm.0209@gmail.com>
2025-08-02 13:03:11 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,12 +1,1 @@
-<?php
-$correct_code = "CODE"; // <-- Ton code ici
 
-if (isset($_GET['code']) && $_GET['code'] === $correct_code) {
-    // Redirection vers la vraie page admin
-    header("Location: ADMIN.html");
-    exit();
-} else {
-    echo "<script>alert('Code incorrect.'); window.location.href = 'index.html';</script>";
-    exit();
-}
-?>
